21bdaf50ad2f6aea0e6d79230bee1ebddf698485
Giovani <giovaniviana2011@gmail.com>
2022-03-17 22:10:04 -0300

    Teste 1

---
+++ Projeto/Campo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int l, c;
+    double Matriz[l][c];
+
+    cout << "______Campo Minado______" << endl;
+    cout << "Escolha o numero de linhas: " << endl;
+    cin << l;
+    cout << "Agora escolha o número de colunas: "
+    cin << c;
+
+    cout << M
+
+  
+    return 0;
+}
