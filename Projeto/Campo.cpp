b25b602a4919a9965aeaaaa25600540c4599fe98
Giovani <giovaniviana2011@gmail.com>
2022-03-24 12:19:10 -0300

    Funções

--- Projeto/Campo.cpp
+++ Projeto/Campo.cpp
@@ -3,27 +3,62 @@
 
 using namespace std;
 
+/* OBJETIVOS: Gerar uma matriz de acordo com a preferencia do jogados
+->Função para sortear bombas
+->Função que verifica a quantidade de bombas vizinhas
+*/
+
+struct Campo { //Informações essenciais para o jogo 
+  int Vadj; //Verifica se a coordenada tem bombas adjacentes/vizinhas
+  int Vbomb; //Verifica se a coordenada tem bombas
+  int Vopen; //Verifica se a coordenada já foi aberta
+  int Vcord; //Verifica se as coordenadas inseridas são válidas
+
+};
+
+int Bombas(int b){ // Função que gera as bombas
+
+}
+
+int ContarVizinhas(int l, int c){
+
+}
+
+int Vizinhas(){
+  for (int i = 0; i < l; i++){
+    for (int j = 0; j < c; j++)
+    {
+      Matriz[l][c].Vadj = 0;
+    }   
+  } 
+}
+  
+
+Campo Matriz[l][c]; //Definição da matriz aonde irá ocorrer o jogo
+
 int main()
 {
-  cout << "______Campo Minado______" << endl;
-  cout << "Escolha o numero de linhas: " << endl;
-
+  int i, j; //Contadores
   int l, c; //Variáveis das linhas e das colunas
-  cin >> l; //leitura da variável do número de linhas	
+  int Qb; //Variável que vai definir a quantidade de bombas
 
-  cout << "Agora escolha o numero de colunas: " << endl;
-  cin >> c; // leitura da variável do número de colunas    
+  cout << "______Campo Minado______" << endl;
+  cout << "Escolha o numero de linhas e colunas: " << endl;
 
-    int i, j; //Contadores
-    double Matriz[l][c]; //definição da matriz aonde irá ocorrer o jogo
+  cin >> l >> c; //leitura da variável do número de linhas e colunas	       
 
-    for (i = 0; i < l; i++){ //Leitura das linhas
-     for (j = 0; j < c; j++){ //Leitura das colunas       
-        cin >> Matriz[l][c];
-        }        
-    }
+  for (i = 0; i < l; i++){ //Leitura das linhas
+    for (j = 0; j < c; j++){ //Leitura das colunas       
+      cin >> Matriz[l][c];
+    }        
+  }
 
-    for (i = 0; i < l; i++) {  // Imprimindo a Matriz
+  cout << "Escolha a quantidade de bombas: "
+  cin >> Qb; //Leitura da qunatidade de bombas
+
+  Bombas(Qb); // Função que aleatoriza as bombas no campo
+
+  for (i = 0; i < l; i++) {  // Imprimindo a Matriz
     for (j = 0; j < c; j++){
       cout << Matriz[l][c] << endl;
     }  
