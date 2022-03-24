1b3407ea2d0f0fe35765b2b093ed11388d637932
Giovani <giovaniviana2011@gmail.com>
2022-03-24 12:13:53 -0300

    att

--- Projeto/Campo.cpp
+++ Projeto/Campo.cpp
@@ -5,17 +5,29 @@ using namespace std;
 
 int main()
 {
-    int l, c;
-    double Matriz[l][c];
+  cout << "______Campo Minado______" << endl;
+  cout << "Escolha o numero de linhas: " << endl;
 
-    cout << "______Campo Minado______" << endl;
-    cout << "Escolha o numero de linhas: " << endl;
-    cin << l;
-    cout << "Agora escolha o número de colunas: "
-    cin << c;
+  int l, c; //Variáveis das linhas e das colunas
+  cin >> l; //leitura da variável do número de linhas	
 
-    cout << M
+  cout << "Agora escolha o numero de colunas: " << endl;
+  cin >> c; // leitura da variável do número de colunas    
 
-  
+    int i, j; //Contadores
+    double Matriz[l][c]; //definição da matriz aonde irá ocorrer o jogo
+
+    for (i = 0; i < l; i++){ //Leitura das linhas
+     for (j = 0; j < c; j++){ //Leitura das colunas       
+        cin >> Matriz[l][c];
+        }        
+    }
+
+    for (i = 0; i < l; i++) {  // Imprimindo a Matriz
+    for (j = 0; j < c; j++){
+      cout << Matriz[l][c] << endl;
+    }  
+  }
+   
     return 0;
 }
