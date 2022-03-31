40a6cfee8060b8d6ba122be77310515cc74bea6f
Giovani <giovaniviana2011@gmail.com>
2022-03-31 12:39:24 -0300

    imopressão tabela e números aleatórios

--- Projeto/Campo.cpp
+++ Projeto/Campo.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<fstream>
+#include <stdlib.h> // Biblioteca rand, srand
 #include <iomanip>
 
 using namespace std;
@@ -9,109 +9,60 @@ using namespace std;
 ->Função que verifica a quantidade de bombas vizinhas
 */
 
-int **le_matriz(int& l, int& c) {
-  ifstream fr;
-  int **v;
-  int i,j;
-
-  fr.open ("m-in.txt");
-
-  if (!fr.is_open()) {
-    cerr << "ERRO ao abrir m-in.txt\n";
-    return 0;  /* retorna ponteiro nulo */
-  }
-
-  fr >> l >> c; //dimensoes da tabela
-
-  v = new int*[l]; //aloca vetor de vetores int
-
-  for (i = 0; i < l; i++)
-    v[i] = new int[c]; //aloca em cada posicao um vetor de int
-
-    for (i = 0; i < l; i++)
-      for (j = 0; j < c; j++)
-        fr >> v[i][j]; //leitura dos elementos
-
-  fr.close();
-
-  return v;
-}
-
-void escreve_matriz(int **v, int l, int c) {
-  ofstream fw;
-  int i, j;
-
-  fw.open("m-out.txt", ios::trunc);
-
-  if (!fw.is_open()) {
-    cerr << "ERRO ao abrir m-out.txt\n";
-    return;  /* Abandona a função */
-  }
-
-  fw << "linhas = " << l << '\n'; /* Dimensão da matriz */
-  fw << "colunas = " << c << '\n';
-
-  for (i = 0; i < l; i++)
-    for (j = 0; j < c; j++)
-      fw << "mat[" << i << "][" << j << "] = " << v[i][j] << '\n';//imprime matriz
-
-  fw.close();
-}
-
 struct Campo { //Informações essenciais para o jogo 
+
   int Vadj; //Verifica se a coordenada tem bombas adjacentes/vizinhas
   int Vbomb; //Verifica se a coordenada tem bombas
   int Vopen; //Verifica se a coordenada já foi aberta
   int Vcord; //Verifica se as coordenadas inseridas são válidas
-
+  
 };
 
 int Bombas(int b){ // Função que gera as bombas
+    int Qb;
+    for (int b= 0; b == Qb; b++) //Gerando número aleátorio 
+        cout << rand() % 10  << "; ";
+        cout << endl;
 
+        return 0;
 }
 
-int ContarVizinhas(int l, int c){
-
-}
-
-int Vizinhas(){
-  for (int i = 0; i < l; i++){
-    for (int j = 0; j < c; j++)
-    {
-      Matriz[l][c].Vadj = 0;
-    }   
-  } 
-}
-  
-
-Campo Matriz[l][c]; //Definição da matriz aonde irá ocorrer o jogo
 
 int main()
 {
-  int **v;
+
   int i, j; //Contadores
   int l, c; //Variáveis das linhas e das colunas
   int Qb; //Variável que vai definir a quantidade de bombas
+  	
+  double Matriz[l][c];
 
   cout << "______Campo Minado______" << endl;
-  cout << "Escolha o numero de linhas e colunas: " << endl;
 
-  v = le_matriz(l, c); //passagem por referencia
-  
-  cout << "Escolha a quantidade de bombas: "
+  cout << "Escolha o numero de linhas e colunas: " << endl;
+  cin >> l >> c; //leitura da variável do número de linhas e colunas	       
+
+  cout << "Escolha a quantidade de bombas: ";
   cin >> Qb; //Leitura da qunatidade de bombas
 
-  Bombas(Qb); // Função que aleatoriza as bombas no campo
+  Bombas(Qb);
 
-  if(v != 0){
-    escreve_matriz(v, l, c);
-    delete[] v;
-    return 0;
-  }
+  for (i = 0; i < l; i++) {  // Imprimindo a Matriz
+  	cout << "|";
+    for (j = 0; j < c; j++){
+      cout << " * ";
+    }
+    cout << "|" << endl;
+  } 
 
-  else {//erro na abertura do arquivo, encerra programa com codigo de erro    
-    return 1;
-  }
-   
-    return 0;
+/* 
+
+'*' -> Não descoberto
+ 'X' -> Bomba
+ '0' -> Vazio ao redor
+ '1' - '8' -> Bombas ao redor
+
+ */
+
+  return 0;
 }
